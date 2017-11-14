7e7a9ecdf4ffb808bcb795c49aadbb1931a9a6c7
m-gb <maya.goldberg90@gmail.com>
2017-11-14 09:42:07 +0100

    computing weekly changes

--- factbook-month.sql
+++ factbook-month.sql
@@ -60,10 +60,57 @@ from /*
       */
       generate_series(date : 'start',
                       date : 'start' + interval '1 month'
-                             - interval '1 day',
+                                     - interval '1 day',
                       interval '1 day'
       )
       as calendar(entry)
       left join factbook
              on factbook.date = calendar.entry
 order by date;
+
+-- Computing weekly changes
+with computed_data as -- fetches the extended data set we need, including the last_week_dollars computed column.
+(
+    select
+      cast(date as date)   as date,
+      to_char(date, 'Dy')  as day,
+      coalesce(dollars, 0) as dollars,
+      lag(dollars, 1)
+      over (
+        partition extract('isodow' from date) -- allows computing the Day Of Week following the ISO rules.
+        order by date
+        )
+                           as last_week_dollars
+    from /*
+          * Generate the month calendar, plus a week before
+          * so that we have values to compare dollars against
+          * even for the first week of the month.
+          */
+          generate_series(date :'start' - interval '1 week',
+                          date :'start' + interval '1 month'
+                                 - interval '1 day',
+                          interval '1 day'
+          )
+        as calendar(date)
+      left join factbook using (date)
+)
+    select date, day,
+           to_char(
+               coalesce(dollars, 0),
+               'L99G999G999G999'
+           ) as dollars,
+           case when dollars is not null
+                and dollars <> 0
+                then round(   100.0
+                            * (dollars - last_week_dollars)
+                            / dollars
+                          , 2)
+           end
+           as "WOW %"
+    from computed_data
+    where date >= date :'start'
+order by date;
+
+
+
+)
