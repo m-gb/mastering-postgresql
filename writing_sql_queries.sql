d0984dcdef5076b37d202be9bae7d611fa7383f8
m-gb <maya.goldberg90@gmail.com>
2017-11-17 09:26:54 +0100

    stored procedures

--- writing_sql_queries.sql
+++ writing_sql_queries.sql
@@ -20,3 +20,54 @@ select name,
 from track
 where albumid = 193
 order by trackid;
+
+-- Stored procedures
+-- This function accepts parameters. To run it: select * from get_all_albums(127);
+create or replace function get_all_albums
+  (
+    in artistid bigint,
+    out album text,
+    out duration interval
+  )
+returns setof record
+language sql
+as $$
+  select album.title as album,
+         sum(milliseconds) * interval '1 ms' as duration
+  from album
+       join artist using(artistid)
+       left join track using(albumid)
+  where artist.artistid = get_all_albums.artistid
+group by album
+order by album;
+$$;
+
+-- If we only have the artist name:
+select *
+  from get_all_albums(
+    (select artistid
+      from artist
+     where name = 'Red Hot Chili Peppers')
+    );
+
+-- Using the query in more complex use cases
+-- Using the function get_all_albums in a join clause using lateral join.
+select album, duration
+  from artist,
+    lateral get_all_albums(artistid)
+where artist.name = 'Red Hot Chili Peppers';
+
+-- Lists the albums (with durations) of the artists who have four albums
+with four_albums as
+(
+  select artistid
+  from album
+  group by artistid
+  having count(*) = 4
+)
+  select artist.name, album, duration
+  from four_albums
+       join artist using(artistid),
+       lateral get_all_albums(artistid)
+  order by artistid, duration desc;
+-- Using stored procedure allows reusing SQL code in between use cases, on the server side.
